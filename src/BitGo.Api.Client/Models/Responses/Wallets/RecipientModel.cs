16f325dfdc9d8a1587e495201b4496203829c790
Ed Wang <ed555009@gmail.com>
2025-06-06 10:54:04 +0800

    feat: update RecipientModel to use decimal for Amount
    - RecipientModel.cs
      - Change Amount property type from string to decimal

--- src/BitGo.Api.Client/Models/Responses/Wallets/RecipientModel.cs
+++ src/BitGo.Api.Client/Models/Responses/Wallets/RecipientModel.cs
@@ -3,6 +3,6 @@ namespace BitGo.Api.Client.Models.Responses.Wallets;
 public class RecipientModel
 {
 	public string? Address { get; set; }
-	public string? Amount { get; set; }
+	public decimal? Amount { get; set; }
 	public string? Data { get; set; }
 }
